d738ab6e24bd733e21e8f6aaaff9e720ccce7cdf
Michelle Rogers <michelleevarogers@gmail.com>
2022-04-23 14:07:21 -0700

    updated application v2.0.0

--- README.md
+++ README.md
@@ -1,9 +1,31 @@
 # WeatherSplash App
 
-URL: [WeatherSplash](https://weathersplash.netlify.app/)
+Weather App (Vanilla JS)
 
-This application uses Vanilla JavaScript and the [OpenWeather Free API](https://openweathermap.org/api) to deliver current weather (in both Celcius and Fahrenheit) based on your current location. It also displays a 5-day forecast with temperatures and conditions every 3 hours.
+Made with modern JavaScript and utilizes the OpenWeatherMap API to deliver current weather (in both Celcius and Fahrenheit) based on your current location (via browser location services). It displays current weather data as well as an 8 day extended forecast. Great for browser home page!
 
-## Build Status
+## Notes
 
-[![Netlify Status](https://api.netlify.com/api/v1/badges/38abd148-31e1-4f18-80b2-176b5dba9aef/deploy-status)](https://app.netlify.com/sites/weathersplash/deploys)
+Be sure to create a src/.env file set the following Environment Variable for development mode as well as production when pushed to server (eg. Netlify...etc).
+
+1. API_KEY (OpenWeatherMap.org API Key)
+
+## Scripts
+
+To start development environment (with WebPack)
+
+> yarn dev
+
+To build application for production
+
+> yarn build
+
+## Tags
+
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![SASS](https://img.shields.io/badge/SASS-hotpink.svg?style=for-the-badge&logo=SASS&logoColor=white)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![Express.js](https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB)
+![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)
+![MongoDB](https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white)
+![Google Cloud](https://img.shields.io/badge/GoogleCloud-%234285F4.svg?style=for-the-badge&logo=google-cloud&logoColor=white)
--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,69 @@
-const webpack = require('webpack');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const path = require('path');
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-  entry: './src/js/app.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/',
-    filename: 'js/[name].bundle.js',
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
   },
-  mode: 'production',
-  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name]-[contenthash].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "build"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
   module: {
     rules: [
       {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
-        enforce: 'pre',
-        use: ['source-map-loader', 'babel-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              esModule: false,
-              outputPath: 'img',
-              name: '[name].[ext]',
-            },
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
           },
-        ],
+        },
       },
       {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: { minimize: true },
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-        ],
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },
   plugins: [
-    new HtmlWebPackPlugin({
-      template: './index.html',
-      filename: './index.html',
-      favicon: './favicon.ico',
+    new HtmlWebpackPlugin({
+      title: "WebPack App",
+      filename: "index.html",
+      template: "src/template.html",
+      favicon: "src/assets/favicon.ico",
     }),
     new MiniCssExtractPlugin({
-      filename: 'css/[name].css',
-      chunkFilename: 'css/[id].css',
+      linkType: "text/css",
+      filename: "index.css",
+      chunkFilename: "[name].css",
     }),
-    new webpack.DefinePlugin({
-      'process.env.API_KEY': JSON.stringify(process.env.API_KEY || 'production'),
-    }),
+    new Dotenv(),
   ],
 };
