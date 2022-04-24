b0dc860f12376489466ba813c3060b059eaf98ba
Michelle Rogers <michelleevarogers@gmail.com>
2022-04-23 22:25:10 -0700

    added server

---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static("build"));
+
+app.get("/", (req, res) => {
+  res.send("WeatherSplash");
+});
+
+app.listen(port, () => {
+  console.log(`> WeatherSplash listening on port: ${port}`);
+});
