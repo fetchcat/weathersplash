911a3513a15e3cfa1fd00505d9a0a4ef1ebdbc83
Michelle Rogers <michelle@merogers.dev>
2023-06-16 14:30:57 -0500

    Remove subdirectory

--- frontend/src/js/util/api.js
+++ frontend/src/js/util/api.js
@@ -1,6 +1,6 @@
 export const getLocation = async (city) => {
   try {
-    const weather = await fetch(`/weathersplash/api?city=${city}`);
+    const weather = await fetch(`/api?city=${city}`);
     const weatherData = await weather.json();
     return weatherData;
   } catch (error) {
@@ -11,9 +11,7 @@ export const getLocation = async (city) => {
 
 export const getByGeolocation = async (latitude, longitude) => {
   try {
-    const weather = await fetch(
-      `/weathersplash/api/geo?lat=${latitude}&lon=${longitude}`
-    );
+    const weather = await fetch(`/api/geo?lat=${latitude}&lon=${longitude}`);
     const weatherData = await weather.json();
     return weatherData;
   } catch (error) {
